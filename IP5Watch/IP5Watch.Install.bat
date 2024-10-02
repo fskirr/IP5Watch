d9f670fbaa038df1ee2266acbdbdcf527360e279
fskirr <68026097+fskirr@users.noreply.github.com>
2024-10-02 10:17:59 -0300

    Update IP5Watch.Install.bat
    Adicionado regra de firewall do Windows para liberar PING

--- IP5Watch/IP5Watch.Install.bat
+++ IP5Watch/IP5Watch.Install.bat
@@ -1,3 +1,4 @@
 sc create "IP5 Watch" binpath=C:\IP5Watch\bin\IP5Watch.exe" --config "C:\IP5Watch\IP5Watch.config.conf start=auto
 netsh advfirewall firewall add rule name="IP5Watch" action=allow protocol=TCP dir=in localport=10050
 netsh advfirewall firewall add rule name="IP5Watch" action=allow protocol=TCP dir=in localport=10051
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
